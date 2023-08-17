939a4ae4745097235bf752be4a37393560245003
Molton321 <molton334@gmail.com>
2023-08-16 23:12:44 -0500

    Initial commit

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+import kivy
+
+def main():
+    saludo()
+    
+
+def saludo():
+    print('Hello, world!')
+
+if __name__ == '__main__':
+    main()
+    
+lambda x: x + 1
+# que es esto? una funcion anonima que recibe un parametro x y le suma 1 y lo devuelve como resultado funciona en caso como     map(lambda x: x + 1, [1, 2, 3]) que devuelve [2, 3, 4]
